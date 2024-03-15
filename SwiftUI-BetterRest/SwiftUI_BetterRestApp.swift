766ae20d51ae9af9f99e1de937027deb29cd7a3d
Jimmy <myname90196@gmail.com>
2024-03-15 09:41:35 +0800

    Initial Commit

---
+++ SwiftUI-BetterRest/SwiftUI_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BetterRestApp.swift
+//  SwiftUI-BetterRest
+//
+//  Created by JimmyChao on 2024/3/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
